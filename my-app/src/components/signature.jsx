623a5a049b6ffeead8af2770140308f70852a787
leonardo75018 <leonardo.kabongo@gmail.com>
2021-03-05 14:33:31 +0100

    -

--- my-app/src/components/signature.jsx
+++ my-app/src/components/signature.jsx
@@ -117,9 +117,10 @@ const Signature = () => {
                     mebers.map(item =>
 
                         <div className="email-card" style={{ width: "300px" }}>
+
                             <div className="logo-content" style={{ display: "flex", justifyContent: "flex-end" }}>
                                 <div className="logo">
-                                    <img src={logo} alt="" style={{ width: "200px" }} />
+                                    <img src="https://firebasestorage.googleapis.com/v0/b/backwidactic.appspot.com/o/logo.png?alt=media&token=f827f8f8-9ab6-403c-a44b-088abd21686f" alt="" style={{ width: "200px" }} />
                                 </div>
                             </div>
 
@@ -138,41 +139,43 @@ const Signature = () => {
 
                                         <a href="https://www.linkedin.com/company/widactic/" target="_blank">
                                             <div className="insta">
-                                                <img src={linkidin} alt="" style={{ width: "25px", paddingRight: "4px" }} />
+                                                <img src="https://firebasestorage.googleapis.com/v0/b/backwidactic.appspot.com/o/linkidin.png?alt=media&token=69caade1-e963-4167-9fcf-a3538923f8d1" alt="" style={{ width: "25px", paddingRight: "4px" }} />
 
                                             </div>
                                         </a>
 
                                         <a href="https://twitter.com/Widactic" target="_blank">
                                             <div className="insta">
-                                                <img src={tweeter} alt="" style={{ width: "25px", paddingRight: "4px" }} />
+                                                <img src="https://firebasestorage.googleapis.com/v0/b/backwidactic.appspot.com/o/tweeter.png?alt=media&token=14dd5ca6-9279-4fec-a24c-3859ffc648ec" alt="" style={{ width: "25px", paddingRight: "4px" }} />
                                             </div>
                                         </a>
 
                                         <a href="https://www.youtube.com/channel/UCLOnRmY2mLOpKEmztASOE-Q" target="_blank">
                                             <div className="insta">
-                                                <img src={youtube} alt="" style={{ width: "25px", paddingRight: "4px" }} />
+                                                <img src="https://firebasestorage.googleapis.com/v0/b/backwidactic.appspot.com/o/youtube.png?alt=media&token=319f7275-0c84-4004-83a7-1a6960d567be" alt="" style={{ width: "25px", paddingRight: "4px" }} />
 
                                             </div>
                                         </a>
 
                                         <a href="https://vimeo.com/widactic" target="_blank">
                                             <div className="insta">
-                                                <img src={vimeo} alt="" style={{ width: "25px", paddingRight: "4px" }} />
+                                                <img src="https://firebasestorage.googleapis.com/v0/b/backwidactic.appspot.com/o/vimeo.png?alt=media&token=75366767-7720-472d-83d6-054c5e09d232" alt="" style={{ width: "25px", paddingRight: "4px" }} />
 
                                             </div>
                                         </a>
 
                                         <a href="https://www.instagram.com/widactic/?hl=fr" target="_blank">
                                             <div className="insta">
-                                                <img src={insta} alt="" style={{ width: "25px", paddingRight: "4px" }} />
+                                                <img src="https://firebasestorage.googleapis.com/v0/b/backwidactic.appspot.com/o/insta.png?alt=media&token=be9e2446-fc8f-4e40-a005-de123d7be203" alt="" style={{ width: "25px", paddingRight: "4px" }} />
 
                                             </div>
                                         </a>
                                     </div>
                                 </div>
                             </div>
+
                         </div>
+
                     )
                 }
 
